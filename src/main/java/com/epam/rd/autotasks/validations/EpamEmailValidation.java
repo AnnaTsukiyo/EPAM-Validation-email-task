69b69f4cb97110c12a9a8c17d3d9d9b948bf86cd
AnnaTsukiyo <annett.sav4enkko@gmail.com>
2022-02-05 21:25:45 +0200

    done

--- src/main/java/com/epam/rd/autotasks/validations/EpamEmailValidation.java
+++ src/main/java/com/epam/rd/autotasks/validations/EpamEmailValidation.java
@@ -1,6 +1,5 @@
 package com.epam.rd.autotasks.validations;
 
-
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -20,39 +19,39 @@ public class EpamEmailValidation {
         return matcher.matches();
     }
 
-    public static void main(String args[]) {
-        EpamEmailValidation validations = new EpamEmailValidation();
-        String first = "william_shakespeare@epam.com";
-        String second = "lu_e@epam.com";
-        String third = "william_shakespeare1@epam.com";
-        String fourth = "william_shakespeare2@epam.com";//true
-        String fifth = "william@epam.com";
-        String sixth = "william.shakespeare@epam.com";
-        String seventh = "william...shakespeare@epam.com";
-        String eighth = "william-shakespeare@epam.com";
-        String ninth = "shakespeare123@epam.com";
-        String tenth = "william_$hakespeare@epam.com";
-        String eleventh = "_shakespeare@epam.com";
-        String twelve = "shakespeare_@epam.com";
-        String thirteenth = "william.shakespeare@epam@com";
-        String fourteenth = "william.shakespeare@epamcom";
-        String fifteenth = " ";
-        System.out.println(validations.validateEpamEmail(first));
-        System.out.println(validations.validateEpamEmail(second));
-        System.out.println(validations.validateEpamEmail(third));
-        System.out.println(validations.validateEpamEmail(fourth));
-        System.out.println(validations.validateEpamEmail(fifth));
-        System.out.println(validations.validateEpamEmail(sixth));
-        System.out.println(validations.validateEpamEmail(seventh));
-        System.out.println(validations.validateEpamEmail(eighth));
-        System.out.println(validations.validateEpamEmail(ninth));
-        System.out.println(validations.validateEpamEmail(tenth));
-        System.out.println(validations.validateEpamEmail(eleventh));
-        System.out.println(validations.validateEpamEmail(twelve));
-        System.out.println(validations.validateEpamEmail(thirteenth));
-        System.out.println(validations.validateEpamEmail(fourteenth));
-        System.out.println(fifteenth);
-    }
+//    public static void main(String args[]) {
+//        EpamEmailValidation validations = new EpamEmailValidation();
+//        String first = "william_shakespeare@epam.com";
+//        String second = "lu_e@epam.com";
+//        String third = "william_shakespeare1@epam.com";
+//        String fourth = "william_shakespeare2@epam.com";//true
+//        String fifth = "william@epam.com";
+//        String sixth = "william.shakespeare@epam.com";
+//        String seventh = "william...shakespeare@epam.com";
+//        String eighth = "william-shakespeare@epam.com";
+//        String ninth = "shakespeare123@epam.com";
+//        String tenth = "william_$hakespeare@epam.com";
+//        String eleventh = "_shakespeare@epam.com";
+//        String twelve = "shakespeare_@epam.com";
+//        String thirteenth = "william.shakespeare@epam@com";
+//        String fourteenth = "william.shakespeare@epamcom";
+//        String fifteenth = " ";
+//        System.out.println(validations.validateEpamEmail(first));
+//        System.out.println(validations.validateEpamEmail(second));
+//        System.out.println(validations.validateEpamEmail(third));
+//        System.out.println(validations.validateEpamEmail(fourth));
+//        System.out.println(validations.validateEpamEmail(fifth));
+//        System.out.println(validations.validateEpamEmail(sixth));
+//        System.out.println(validations.validateEpamEmail(seventh));
+//        System.out.println(validations.validateEpamEmail(eighth));
+//        System.out.println(validations.validateEpamEmail(ninth));
+//        System.out.println(validations.validateEpamEmail(tenth));
+//        System.out.println(validations.validateEpamEmail(eleventh));
+//        System.out.println(validations.validateEpamEmail(twelve));
+//        System.out.println(validations.validateEpamEmail(thirteenth));
+//        System.out.println(validations.validateEpamEmail(fourteenth));
+//        System.out.println(fifteenth);
+//    }
 }
 
 
