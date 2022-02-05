a748c62f698770fd2b213525d1b1ab825511b171
AnnaTsukiyo <annett.sav4enkko@gmail.com>
2022-02-05 23:15:35 +0200

    done

--- src/main/java/com/epam/rd/autotasks/validations/EpamEmailValidation.java
+++ src/main/java/com/epam/rd/autotasks/validations/EpamEmailValidation.java
@@ -15,7 +15,7 @@ public class EpamEmailValidation {
         if (matcher.matches()) {
             return true;
         } if (email == null || email.isEmpty() || " ".equals(email)){
-            throw new NullPointerException();
+            return false;
         }else{
             return false;
         }
